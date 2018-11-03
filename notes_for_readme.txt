317b2376e0814a99fc1866e4acd3789b9f67dc14
Adam Davies <ahdavies6@gmail.com>
2018-11-03 11:07:45 -0600

    played around more with CoreNLP parser; added notes_for_readme

---
+++ notes_for_readme.txt
@@ -0,0 +1,12 @@
+External Resources (see install.txt):
+    Numpy
+    NLTK
+    Stanford CoreNLP
+
+
+Contributions:
+    Adam:
+        designed project structure, picked (most of) the tools we'll use
+        found/configured NLTK, numpy, Stanford CoreNLP/Parser/NE/etc.
+        set up Stanford CoreNLP, git, jupyter
+        learned NLTK; built chunker, parser, etc.
--- syntax/parse.py
+++ syntax/parse.py
@@ -20,7 +20,43 @@ from nltk.parse.corenlp import CoreNLPParser
 #     pass
 
 
+# def parse_for(tree, label):
+#     if tree.label() == label:
+#         return tree
+#     else:
+#         for child in tree:
+#             if child.label() == label:
+#                 return child
+#             else:
+#                 return parse_for(child, label)
+
+
 tokens = nltk.word_tokenize("Why did George get on the bus?")
 parsed = CoreNLPParser().parse(tokens)
 constituent_trees = [x for x in parsed][0]
-print(constituent_trees)
+
+# print(constituent_trees)
+# for tree in constituent_trees:
+#     assert isinstance(tree, nltk.Tree)
+#     # print(tree.label())
+#
+#     for subtree in tree:
+#         # print(subtree.label())
+#
+#         for x in subtree:
+#             if isinstance(x, nltk.Tree):
+#                 print(x.label())
+#
+#                 for y in x:
+#                     if isinstance(y, nltk.Tree):
+#                         print(y.label())
+#
+#                         for z in y:
+#                             if isinstance(z, nltk.Tree):
+#                                 print(z.label())
+#
+#                                 for w in z:
+#                                     if isinstance(w, nltk.Tree):
+#                                         assert isinstance(w, nltk.Tree)
+#                                         print(w.label())
+#                                         print(w.leaves())
