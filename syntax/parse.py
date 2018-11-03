cf25e0e4dc3730b2b3c2e97c4b269b1479a56456
Adam Davies <ahdavies6@gmail.com>
2018-11-03 16:52:33 -0600

    completed parser; added unit tests which contain logic for the answer ID system

--- syntax/parse.py
+++ syntax/parse.py
@@ -1,62 +1,85 @@
-import nltk
+from nltk import Tree
 from nltk.parse.corenlp import CoreNLPParser
 
 
-# def question_to_proposition(question):
-#     subject = None
-#     verb = None
-#     verb_arg = None
-#     for word, tag in tagged:
-#         if tag[0] == 'V':
-#             assert verb is None
-#             verb = word
-#         elif (
-#             tag[0:2] == 'PR' or     # pronoun
-#             tag == "NNP"            # singular proper noun
-#         ):
-#             if subject is None:
-#                 subject = word
-#
-#     pass
+def parse_for(tree, label):
+    """
+    :param tree: The parse tree (typically beginning at ROOT)
+    :param label: The non-terminal label
+    :return: Returns a list of Tree representations of all non-terminals in the tree with the specified label.
+        If none can be found, returns None
+    """
+    trees = []
+    if tree.label() == label:
+        trees.append(tree)
+    else:
+        for child in tree:
+            if isinstance(child, Tree):
+                grandchildren = parse_for(child, label)
+                if grandchildren is not None:
+                    for grandchild in grandchildren:
+                        trees.append(grandchild)
+    return trees if trees else None
 
 
-# def parse_for(tree, label):
-#     if tree.label() == label:
-#         return tree
-#     else:
-#         for child in tree:
-#             if child.label() == label:
-#                 return child
-#             else:
-#                 return parse_for(child, label)
+# unittest portion
+import unittest
 
 
-tokens = nltk.word_tokenize("Why did George get on the bus?")
-parsed = CoreNLPParser().parse(tokens)
-constituent_trees = [x for x in parsed][0]
+class MyTestCase(unittest.TestCase):
+    def test_multiple(self):
+        sentence = "George and Mary ate dinner."
+        parsed = next(CoreNLPParser().raw_parse(sentence))
+        self.assertEqual(
+            (parse_for(parsed, "NNP")[0].leaves()[0], parse_for(parsed, "NNP")[1].leaves()[0]),
+            ("George", "Mary")
+        )
+        self.assertIsNone(parse_for(parsed, "NOPE"))
 
-# print(constituent_trees)
-# for tree in constituent_trees:
-#     assert isinstance(tree, nltk.Tree)
-#     # print(tree.label())
-#
-#     for subtree in tree:
-#         # print(subtree.label())
-#
-#         for x in subtree:
-#             if isinstance(x, nltk.Tree):
-#                 print(x.label())
-#
-#                 for y in x:
-#                     if isinstance(y, nltk.Tree):
-#                         print(y.label())
-#
-#                         for z in y:
-#                             if isinstance(z, nltk.Tree):
-#                                 print(z.label())
-#
-#                                 for w in z:
-#                                     if isinstance(w, nltk.Tree):
-#                                         assert isinstance(w, nltk.Tree)
-#                                         print(w.label())
-#                                         print(w.leaves())
+    # todo: migrate some of the logic from this method into the answer identification code
+    def test_q_to_a(self):
+        # parse question
+        question = "Where did Fred find the cookies?"
+        q_parsed = next(CoreNLPParser().raw_parse(question))
+        q_named = parse_for(q_parsed, "NNP")
+        q_vp = parse_for(q_parsed, "VP")
+        q_vb = parse_for(q_vp[0], "VB")
+        q_obj = parse_for(q_vp[0], "NP")
+        self.assertEqual(1, len(q_named))
+        self.assertEqual("Fred", " ".join(q_named[0].leaves()))
+        self.assertEqual(1, len(q_vp))
+        self.assertEqual("find the cookies", " ".join(q_vp[0].leaves()))
+        self.assertEqual(1, len(q_vb))
+        self.assertEqual("find", " ".join(q_vb[0].leaves()))
+        self.assertEqual(1, len(q_obj))
+        self.assertEqual("the cookies", " ".join(q_obj[0].leaves()))
+
+        # parse answer
+        answer = "Fred found the cookies in the cupboard."
+        a_parsed = next(CoreNLPParser().raw_parse(answer))
+        a_named = parse_for(a_parsed, "NNP")
+        a_vp = parse_for(a_parsed, "VP")
+        a_vbd = parse_for(a_vp[0], "VBD")
+        a_nps = parse_for(a_vp[0], "NP")
+        a_pp = parse_for(a_vp[0], "PP")
+        self.assertEqual(1, len(a_named))
+        self.assertEqual("Fred", " ".join(a_named[0].leaves()))
+        self.assertEqual(1, len(a_vp))
+        self.assertEqual("found the cookies in the cupboard", " ".join(a_vp[0].leaves()))
+        self.assertEqual(1, len(a_vbd))
+        self.assertEqual("found", " ".join(a_vbd[0].leaves()))
+        self.assertEqual(2, len(a_nps))
+        self.assertEqual("the cookies", " ".join(a_nps[0].leaves()))
+        self.assertEqual("the cupboard", " ".join(a_nps[1].leaves()))
+        self.assertEqual(1, len(a_pp))
+        self.assertEqual("in the cupboard", " ".join(a_pp[0].leaves()))
+
+        # "find" (verify) answer of question
+        self.assertEqual(q_named, a_named)
+        # self.assertEqual(q_vbd, a_vbd)     # but past tense, though!
+        self.assertEqual(q_obj[0], a_nps[0])
+        # and PP is the answer, since we're dealing with "where"!
+
+
+if __name__ == '__main__':
+    unittest.main()
