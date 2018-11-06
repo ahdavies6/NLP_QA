96b4434ac6001622bac724759effda159f9f085c
carlo-33 <cjsaltlake@gmail.com>
2018-11-06 14:38:05 -0700

    Revert "output to files for testing/answer-key"
    This reverts commit 458e844

--- qa2.py
+++ qa2.py
@@ -12,10 +12,9 @@ date_pattern = r'DATE:\s*(.*)\n'
 story_pattern = r'STORYID:\s*(.*)\n'
 text_pattern = r'TEXT:\s*([\s\w\d\S\W\D]*)'
 question_pattern = r'QuestionID:\s*(.*)\s*Question:\s*(.*)\s*Difficulty:\s*(.*)\s*'
-answer_pattern = r'QuestionID:\s*(.*)\s*Question:\s*(.*)\s*Answer:\s*(.*)\s*'
 
 
-def form_output(story, inquiry, questionID):
+def print_answer(story, inquiry, questionID):
     q_inquiry = formulate_question(inquiry)
     qword = q_inquiry['qword'][0].lower()
     if qword == 'who':
@@ -39,19 +38,21 @@ def form_output(story, inquiry, questionID):
     #     feedback = get_prospects_with_lemmatizer2(story, inquiry)
 
     # print('Question: ' + inquiry)
-    output = 'QuestionID: ' + questionID + '\n'
-    output += 'Answer: '
+    print('QuestionID: ' + questionID)
     if len(feedback) > 0:
         best_sentence = heapq.heappop(feedback)[1]
         answer = get_answer_phrase(inquiry, best_sentence)
-        output += answer
-    output += '\n\n'
+        print('Answer: ' + answer)
+    else:
+        print()
     # for x in range(0, 3):
     #     if len(feedback) > 0:
     #         best_sentence = ''.join(heapq.heappop(feedback)[1])
     #         print('Answer: ' + best_sentence)
 
-    return output
+    print()
+
+    x=1
 
 
 if __name__ == '__main__':
@@ -82,22 +83,13 @@ if __name__ == '__main__':
         text = re.search(text_pattern, story).group(1)
         story_files[id] = (headline, date, text.replace('\n', ' '))
 
-    output_file = open('test_output_1.txt', 'w+')
     for id in story_ids:
         question_filename = stories_filename + id + '.questions'
         questions = open(question_filename, 'r+')
         text = questions.read()
         question = re.findall(question_pattern, text)
         for match in question:
-            output = form_output(story_files[id][2], match[1], match[0])        # match[1] is question itself, match[0] is questionID
-            output_file.write(output)
+            print_answer(story_files[id][2], match[1], match[0])        # match[1] is question itself, match[0] is questionID
+            print()
 
-    output_file.close()
 
-    answer_key_file = open('answer_key_1.txt', 'w+')
-    for id in story_ids:
-        answers_filename = stories_filename + id + '.answers'
-        answer_text = open(answers_filename, 'r+')
-        text = answer_text.read()
-        answer_key_file.write(text)
-    answer_key_file.close()
