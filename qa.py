66a881b0f7e5a3efe8388fa667d60015ba6d7383
carlo-33 <cjsaltlake@gmail.com>
2018-11-06 20:53:56 -0700

    Add qa.sh

--- qa.py
+++ qa.py
@@ -1,7 +1,5 @@
-import re
 import sys
 import heapq
-import nltk
 from text_analyzer import *
 from question_classifier import formulate_question
 from answer_identification import get_answer_phrase
---
+++ qa.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+python3 qa.py $1
+exit
