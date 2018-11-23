9c321a35c963be745f5e856f6620f03e0e4b7484
Adam Davies <ahdavies6@gmail.com>
2018-11-23 02:02:09 -0700

    built corpus_io; refactored test_answer_id and qa_test

--- test/test_answer_id.py
+++ test/test_answer_id.py
@@ -6,12 +6,13 @@ from sys import argv
 from nltk import sent_tokenize
 from answer_identification import calculate_overlap, get_answer_phrase
 from text_analyzer import lemmatize
+from corpus_io import Corpus
 
 
 headline_pattern = r'HEADLINE:\s*(.*)\n'
 date_pattern = r'DATE:\s*(.*)\n'
 text_pattern = r'TEXT:\s*([\s\w\d\S\W\D]*)'
-real_answer_pattern = r'QuestionID:\s*(.*)\s*Question:\s*(.*)\s*Answer:\s*(.*)\s*Difficulty:\s*(.*)\s*'
+answer_pattern = r'QuestionID:\s*(.*)\s*Question:\s*(.*)\s*Answer:\s*(.*)\s*Difficulty:\s*(.*)\s*'
 
 
 def get_sentence_with_answer(story, answer):
@@ -32,10 +33,14 @@ def get_sentence_with_answer(story, answer):
 def form_output(story, inquiry, answer, question_id):
     output = 'QuestionID: ' + question_id + '\n'
     output += 'Answer: '
-    best_sentence = get_sentence_with_answer(story, answer[0])
-    answer = get_answer_phrase(inquiry, best_sentence)
-    if answer:
-        output += answer
+    best_sentence = get_sentence_with_answer(
+        story,
+        max(answer, key=lambda x: len(x.split()))
+    )
+    if best_sentence:
+        answer = get_answer_phrase(inquiry, best_sentence)
+        if answer:
+            output += answer
     output += '\n\n'
 
     return output
@@ -71,44 +76,65 @@ def get_random_items(iterable, num_items=25, seed=None):
     return items
 
 
-def main(random_seed, num_tests):
-    story_ids = get_random_items(get_all_ids(), num_tests, random_seed)
-    story_files = {}
+def main(random_seed, num_stories):
+    # story_ids = get_random_items(get_all_ids(), num_stories, random_seed)
+    # story_files = {}
+    #
+    # for story_id in story_ids:
+    #     story_filename = story_id + '.story'
+    #     with open(story_filename, 'r+') as story_file:
+    #         story = story_file.read()
+    #     headline = re.search(headline_pattern, story).group(1)
+    #     date = re.search(date_pattern, story).group(1)
+    #     text = re.search(text_pattern, story).group(1)
+    #     story_files[story_id] = (headline, date, text.replace('\n', ' '))
+    #
+    # with open('output', 'w+') as output_file:
+    #     for story_id in story_ids:
+    #         answer_filename = story_id + '.answers'
+    #         with open(answer_filename, 'r+') as answer_file:
+    #             text = answer_file.read()
+    #         answer_tuples = re.findall(answer_pattern, text)
+    #         for question_id, question, answers, _ in answer_tuples:
+    #             try:
+    #                 output = form_output(
+    #                     story_files[story_id][2],
+    #                     question,
+    #                     [a.strip() for a in answers.split(r'|')],
+    #                     question_id
+    #                 )
+    #             except ConnectionError:
+    #                 print('Server was inaccessible.')
+    #                 raise SystemExit
+    #             output_file.write(output)
+    #
+    # with open('key', 'w+') as answer_key_file:
+    #     for story_id in story_ids:
+    #         answers_filename = story_id + '.answers'
+    #         answer_text = open(answers_filename, 'r+')
+    #         text = answer_text.read()
+    #         answer_key_file.write(text)
+    #
+    # subprocess.run(['perl', 'score_answers.pl', 'output', 'key'])
+    # os.remove('output')
+    # os.remove('key')
 
-    for story_id in story_ids:
-        story_filename = story_id + '.story'
-        with open(story_filename, 'r+') as story_file:
-            story = story_file.read()
-        headline = re.search(headline_pattern, story).group(1)
-        date = re.search(date_pattern, story).group(1)
-        text = re.search(text_pattern, story).group(1)
-        story_files[story_id] = (headline, date, text.replace('\n', ' '))
+    corpus = Corpus()
+    output = ''
+    key = ''
+    for story in corpus.get_random_stories(num_stories, random_seed):
+        story_text = story['text']
+        for question_id in story:
+            if question_id not in ['text', 'answer_key']:
+                question = story[question_id][0]
+                answers = story[question_id][1]
+                output += form_output(story_text, question, answers, question_id)
+        key += story['answer_key']
 
-    with open('output', 'w+') as output_file:
-        for story_id in story_ids:
-            answer_filename = story_id + '.answers'
-            with open(answer_filename, 'r+') as answers:
-                text = answers.read()
-            answer_tuples = re.findall(real_answer_pattern, text)
-            for question_id, question, answer, _ in answer_tuples:
-                try:
-                    output = form_output(
-                        story_files[story_id][2],
-                        question,
-                        [a.strip() for a in answer.split(r'|')],
-                        question_id
-                    )
-                except ConnectionError:
-                    print('Server was inaccessible.')
-                    raise SystemExit
-                output_file.write(output)
-
-    with open('key', 'w+') as answer_key_file:
-        for story_id in story_ids:
-            answers_filename = story_id + '.answers'
-            answer_text = open(answers_filename, 'r+')
-            text = answer_text.read()
-            answer_key_file.write(text)
+    with open('output', 'w') as output_file:
+        output_file.write(output)
+    with open('key', 'w') as answer_key:
+        answer_key.write(key)
 
     subprocess.run(['perl', 'score_answers.pl', 'output', 'key'])
     os.remove('output')
